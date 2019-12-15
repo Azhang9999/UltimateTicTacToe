ae508fa2e7dd86545e0007418cde759507113b3b
Azhang9999 <48506949+Azhang9999@users.noreply.github.com>
2019-12-14 23:20:23 -0800

    random runner and statistical runner mostly modified

--- GameMechanism.py
+++ GameMechanism.py
@@ -111,15 +111,19 @@ class BetterNormalGame(object):
 
     def determineWinner(self):
         """ Returns the winner (self.circle or self.cross) of the game or None """
+        taken_board = {}
         for item in self.winning:
             score = 0
             for board in item:
                 if isinstance(self.universe[board], int):
+                    taken_board[board] = int
                     score += board
             if score == 3 * self.cross:
                 return self.cross
             elif score == 3 * self.circle:
                 return self.circle
+        if len(taken_board) == 9:
+            return 0
         return None
 
     def move(self, side, coordinate):
@@ -146,215 +150,21 @@ class BetterNormalGame(object):
         board - the board that has to be placed on
         """
         assert self.determineWinner() == None, "{} already won".format(BetterNormalGame.convert_side_int_to_str(self.determineWinner()))
-        if self.must_move_board != None and isinstance(self.universe[board], int):
+        if self.must_move_board != None and isinstance(self.universe[inp[0]], int):
             self.must_move_board = None
         if self.must_move_board != None:
             assert inp[0] == self.must_move_board
         self.move(side, inp)
 
 
-#@deprecated("this class is deprecated")
-class NormalGame(object):
-
-    def __init__(self, empty = [], cross = [], circle = [], xDict = {1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[]}
-                 , yDict = {1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[]}, freeBoards = [], xBoards = [], oBoards = []):
-        """ Initiates an empty board of ultimate tic tac toe
-        Each coordinate is expressed by an integer from 0 to 9
-        Each board is repressented by an integer from 0 to 9
-        Each coordinates would be represented by a tuple
-        (board number, square number)
-
-        There is the following attributes:
-
-        self.empty : a list of all of the empty squares
-        self.cross : a list of all squares occupied by x team
-        self.circle : a list of all square occupied by o team
-        self.freeBoards : a list of all boards that can be maneuvered into
-        self.xBoards: a list of all boards taken by x
-        self.oBoards: a list of all boards taken by o
-        self.winning: a list of all of the combinations that result in a win
-        """
-        self.empty = empty.copy()
-        if len(self.empty) == 0:
-            for x in range(1, 10):
-                for y in range(1, 10):
-                    self.empty.append((x,y))
-
-        self.cross = cross.copy()
-        self.circle = circle.copy()
-        self.xDict = copy.deepcopy(xDict)
-        self.yDict = copy.deepcopy(yDict)
-
-        self.freeBoards = freeBoards.copy()
-        if len(self.freeBoards) == 0:
-            for x in range(1, 10):
-                self.freeBoards.append(x)
-
-        self.xBoards = xBoards.copy()
-        self.oBoards = oBoards.copy()
-
-        self.winning = [(1,2,3),(1,4,7),(1,5,9),(2,5,8),(3,5,7),(3,6,9),
-                        (4,5,6),(7,8,9)]
-
-    def copy(self):
-        return NormalGame(self.empty.copy(), self.cross.copy(), self.circle.copy(), copy.deepcopy(self.xDict), copy.deepcopy(self.yDict), self.freeBoards.copy()
-                          , self.xBoards.copy(), self.oBoards.copy())
-
-    def get_circle(self):
-        return self.circle.copy()
-
-    def get_cross(self):
-        return self.cross.copy()
-
-    def show_row(self, row, board):
-        """ returns the row of the board in the game"""
-        cross = self.xDict[board]
-        circle = self.yDict[board]
-        line = ""
-        for x in range(0,3):
-            check = 3*row + x + 1
-            line += "|"
-            if check in cross:
-                line += "X"
-            elif check in circle:
-                line += "O"
-            else:
-                line += " "
-        return line + "|"
-
-    def show_board(self, board):
-        """ prints the position of everyone on the board"""
-
-        answer = ""
-        for x in range(0,3):
-            answer += "-------\n"
-            answer += show_row(x, board) +"\n"
-        answer += "-------"
-        return answer
-
-    def show_game(self):
-        """ Displays the whole layout of the game"""
-        answer = ""
-        for x in range(0,3):
-            # x determins the big row number
-            answer += "\n-------    -------    -------\n"
-            for y in range(0,3):
-                # y determins the rwo number
-                for z in range(0,3):
-                    # z determins the board number
-                    answer += self.show_row((y), (x*3+z+1))+ "    "
-                answer += "\n-------    -------    -------\n"
-            answer += "\n"
-        return answer
-
-
-    def xMove(self, coordinate):
-        """
-        Assuming that the coordinate is in self.empty
-        move it to self.cross
-        returns the board of the next move
-        """
-        assert (coordinate in self.empty and coordinate not in self.cross)
-        self.cross.append(coordinate)
-        self.empty.remove(coordinate)
-        self.xDict[coordinate[0]].append(coordinate[1])
-        self.determineOwnership(1, coordinate[0])
-        if coordinate[1] not in self.freeBoards:
-            return None
-        return coordinate[1]
-
-    def oMove(self, coordinate):
-        """
-        Assuming that the coordinate is in self.empty
-        move it to self.circle
-        returns the board of the next move
-        """
-        assert (coordinate in self.empty and coordinate not in self.circle)
-        self.circle.append(coordinate)
-        self.empty.remove(coordinate)
-        self.yDict[coordinate[0]].append(coordinate[1])
-        self.determineOwnership(0, coordinate[0])
-        if coordinate[1] not in self.freeBoards:
-            return None
-        return coordinate[1]
-
-    def determineOwnership(self, side, board):
-        """
-        determine if anyone owns the board
-        if ownership established
-        move the board from freeBoards to owned boards of a particular side
-
-        self - the game instance
-        board - (int) the game instance
-        side - 1 or 0 decides the side that is being checked
-        """
-        if side%2 == 1:
-            if board in self.xBoards:
-                return True
-            played = self.xDict[board]
-        else:
-            if board in self.oBoards:
-                return True
-            played = self.yDict[board]
-        if board not in self.freeBoards:
+    def is_coord_valid(self, coordinate):
+        if isinstance(self.universe[coordinate[0]], int):
             return False
-        for element in self.winning:
-            answer = True
-            for number in element:
-                if number not in played:
-                    answer = False
-            if answer == True:
-                self.freeBoards.remove(board)
-                if side%2 == 1:
-                    self.xBoards.append(board)
-                else:
-                    self.oBoards.append(board)
-                return True
-        full = True
-        for element in self.empty:
-            if element[0] == board:
-                full = False
-        if full:
-            self.freeBoards.remove(board)
-        return False
-
-    def determineWinner(self):
-        """ Returns the winner  ('x' or 'o') of the game or None if there is none"""
-        for element in self.winning:
-            cross = True
-            circle = True
-            for board in element:
-                if board not in self.xBoards:
-                    cross = False
-                if board not in self.oBoards:
-                    circle = False
-            if cross == True:
-                return 'x'
-            elif circle == True:
-                return 'o'
-        return None
-
-    def next_step(self, side, inp, board = None):
-        """
-        Plays the next step for the game
-        returns the board choice of the next move or None if free reign
-        (need to determine winner and ownership)
-
-        side - 1('x') or 0('o') denoting the side that is playing
-        inp - (<int>,<int>) denoting the coordinate that is being played
-        board - the board that has to be placed on
-        """
-        assert self.determineWinner() == None, "{} already won".format(self.determineWinner())
-        if board != None and board not in self.freeBoards:
-            board = None
-        if board != None:
-            assert inp[0] == board
-        if side%2 == 1:
-            return self.xMove(inp)
-        else:
-            return self.oMove(inp)
-
-
+        if self.universe[coordinate[0]][coordinate[1]] != 0:
+            return False
+        if self.must_move_board != None and coordinate[0] != self.must_move_board:
+            return False
+        return True
 
 class GameRunner(object):
     """ skeleton code for the program that runs the game,
@@ -368,31 +178,23 @@ class GameRunner(object):
         self.game = NormalGame()
 
 
-
-
 class RandomRunner(GameRunner):
     """ Computer plays against itself using random integers"""
 
     def run_game(self):
-        coordinate = (random.randint(1, 9), random.randint(1, 9))
+        game = BetterNormalGame()
+        coordinate = (random.randint(0, 8), random.randint(0, 8))
         side = 1
-        board = None
-        while self.game.determineWinner() == None and len(self.game.freeBoards) > 0:
-            board = self.game.next_step(side%2, coordinate, board)
-            if self.game.determineOwnership(side%2, coordinate[0]):
-                board = None
-            if board not in self.game.freeBoards:
-                board = None
-            side += 1
-            if len(self.game.freeBoards) == 0:
-                break
-            if board == None:
-                while coordinate not in self.game.empty or coordinate[0] not in self.game.freeBoards:
-                    coordinate = (random.randint(1,9), random.randint(1, 9))
-            else:
-                while coordinate not in self.game.empty:
-                    coordinate = (board, random.randint(1, 9))
-        return (self.game.determineWinner(), self.game.cross, self.game.circle)
+        while self.game.determineWinner() == None:
+            board = self.game.get_must_move_board()
+            coordinate = None
+            while coordinate != None and not game.is_coord_valid(coordinate):
+                coordinate = (board, random.randint(0, 8))
+                if board != None:
+                    coordinate = (random.randint(0, 8), coordinate[1])
+            game.next_step(side, coordinate)
+            side = -1 * side
+        return (self.game.determineWinner())
 
 
 def statistical_runner():
@@ -401,7 +203,7 @@ def statistical_runner():
     for y in range(0,100):
         print (y, o, x)
         runner = RandomRunner()
-        if runner.run_game()[0] == 'x':
+        if runner.run_game()[0] == BetterNormalGame.cross:
             x += 1
         else:
             o += 1
@@ -534,5 +336,4 @@ class TreeRunner(GameRunner):
 ##print(game.determineOwnership(2, 'x'))
 
 if __name__ == "__main__":
-    g = BetterNormalGame()
-    print(g.show_game())
+    statistical_runner()
--- PvP.py
+++ PvP.py
@@ -9,24 +9,22 @@ def new_game():
 
 def game_runner():
     game = BetterNormalGame()
-    side = 1
-    print (game.determineWinner())
-    while game.determineWinner()==None:
-        print ("hewwo")
-        print(game.show_game())
+    side = game.cross
+    while game.determineWinner() == None:
+        print (game.show_game())
         if game.get_must_move_board() == None:
             print ('you can move in any board')
             inp = input('please move')
             inp = eval(inp)
-            game.next_step(side, inp)
         else:
             inp = (10, 10)
             while inp[0] != game.get_must_move_board():
                 print ('you can move in the board', game.get_must_move_board())
                 inp = input('please move')
                 inp = eval(inp)
-                game.next_step(side, inp)
+        game.next_step(side, inp)
         side = -1 * side
+    print (BetterNormalGame.convert_side_int_to_str(game.determineWinner()))
 
 if __name__ == "__main__":
     new_game()
